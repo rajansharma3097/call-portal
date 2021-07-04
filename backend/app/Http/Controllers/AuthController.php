3f97b9ef7a6c9ccb4bb110dd73f90ba9bdb9629c
bkanwasi <bkanwasi21@gmail.com>
2021-07-04 12:40:36 +0530

    Fixed Registration issues

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -1,24 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\User;
+use Exception;
+
 class AuthController extends Controller
 {
     function signup(Request $request)
     {
-        $validateData = $request->validate([
+        try {
+            $validateData = $request->validate([
 
-            'name'     => 'required',
-            'email'    => 'email | required | unique:users',
-            'password' => 'required'
-        ]);
-        $validateData['password'] = Hash::make($validateData['password']);
-        $validateData['role_id']  =  3;
-        $user        = User::create($validateData);
-        $accessToken = $user->createToken('authToken')->accessToken;
-        return response(['user' => $user, 'access_token' => $accessToken]);
+                'name'     => 'required',
+                'email'    => 'email | required | unique:users',
+                'password' => 'required'
+            ]);
+            $validateData['password'] = Hash::make($validateData['password']);
+            $validateData['role_id']  =  3;
+            $user        = User::create($validateData);
+            if($user){
+                $accessToken = $user->createToken('authToken')->accessToken;
+                return response(['user' => $user, 'access_token' => $accessToken]);
+            }else{
+                return response()->json(['code' => 2, 'message' => "Somthing went wrong, Please try after sometime." . PHP_EOL]);  
+            }
+            
+        } catch (Exception $e) {
+            return response()->json(['code' => 2, 'message' => $e->getMessage() . PHP_EOL]);
+        }
     }
 }
